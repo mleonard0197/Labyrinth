e0dc176418bbb146747c1eacbb8b1ec174d2ac44
mleonard0197 <33047210+mleonard0197@users.noreply.github.com>
2020-10-12 13:04:34 -0400

    Removed problem script

--- Assets/Scripts/CharControl.cs
+++ Assets/Scripts/CharControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 3DCharacterController : MonoBehaviour
+public class CharControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
