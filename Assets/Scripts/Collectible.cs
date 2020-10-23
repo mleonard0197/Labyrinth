60452d564612343c07e2464f8c070473e14cb1f6
mleonard0197 <33047210+mleonard0197@users.noreply.github.com>
2020-10-23 15:35:33 -0400

    Score system and collectable script

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    //place on score collectables to play the sound and add to the score
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        collectSound.Play();
+        ScoreSystem.theScore += 50;   
+        Destroy(gameObject);
+    }
+}
