8d7bd3307a86fec13bbdd8493d0373bb330542c6
mleonard0197 <33047210+mleonard0197@users.noreply.github.com>
2020-10-26 16:02:48 -0400

    DOTween & Knockback

--- Assets/Scripts/AliveControl.cs
+++ Assets/Scripts/AliveControl.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class AliveControl : MonoBehaviour
 {
     //place on score collectables to play the sound and add to the score
     public AudioSource hurtSound;
     //public Health livesC;
-    //public Rigidbody rb;
+    public Rigidbody rb;
+    
+
 
     public static bool invincible = false;
+
+    //public Vector3 teleportPos;
+
+    public Transform teleportPos;
 
     void OnTriggerEnter(Collider other) 
     {
@@ -27,21 +34,28 @@ public class AliveControl : MonoBehaviour
                 {
                     if (this.gameObject.tag == "Spikes")
                     {
-                    Health.live -= 1;
-                    hurtSound.Play();
-                    //rb.AddForce(transform.forward * -20.0f);
-                    //FindObjectOfType<ThirdPersonMovement>().Knockback();
-                    invincible = true;
-                    Invoke("resetInvulnerability", 2);
+                        Health.live -= 1;
+                        hurtSound.Play();
+                        //rb.AddForce(transform.forward * -20.0f);
+                        //FindObjectOfType<ThirdPersonMovement>().Knockback();
+                        invincible = true;
+                        Invoke("resetInvulnerability", 2);
                     }
                     else 
                     {
-                    Health.live -= 1;
-                    hurtSound.Play();
-                    //rb.AddForce(transform.forward * -20.0f);
-                    FindObjectOfType<ThirdPersonMovement>().Knockback();
-                    invincible = true;
-                    Invoke("resetInvulnerability", 2);
+                        Health.live -= 1;
+                        hurtSound.Play();
+                        //rb.AddForce(transform.forward * -20.0f);
+                        //FindObjectOfType<ThirdPersonMovement>().Knockback();
+                        //rb.AddExplosionForce(300000, transform.position, 10000);
+                        invincible = true;
+                        Invoke("resetInvulnerability", 2);
+
+                        //Moves player to teleportPos after being hit
+                        //**ADD FUNCTIONALITY TO DISABLE THE MESH RENDERED, ENABLE GLITTER/SPRITE FX UNTIL PLAYER REACHES POSITION, THEN ENABLE RENDERER
+                        rb.isKinematic = true;
+                        other.transform.DOMove(teleportPos.position, 2);
+
                     }
                 }
         
@@ -52,6 +66,7 @@ public class AliveControl : MonoBehaviour
 
     void resetInvulnerability()
     {
+        rb.isKinematic = false;
         invincible = false;
     }
 }
